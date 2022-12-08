bb5029672474c5211e35c9a57c3922c0a8bbfed4
Ronak Sankhala <Ronnie@Ronaks-MacBook-Pro.local>
2022-12-08 13:39:16 +0530

    Updated File structure and added few required ViewControllers

--- TeamsChat/TeamsChat/Controller/ConversationsViewController.swift
+++ TeamsChat/TeamsChat/Controller/ConversationsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
     
     var dataViewModel = DataViewModel()
 
---
+++ TeamsChat/TeamsChat/Controller/NewConversationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewConversationViewController.swift
+//  TeamsChat
+//
+//  Created by Ronak Sankhala on 08/12/22.
+//
+
+import UIKit
+
+class NewConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
